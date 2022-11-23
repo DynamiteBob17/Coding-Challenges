61f9b2f768bb11f2a990751b864c72dadb734e31
Jan Mlinarić <jan.mlinaric@gmail.com>
2022-11-24 00:38:28 +0100

    fix: don't mutate the array received by the function

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,4 +1,6 @@
-function bubbleSort(array) {
+function bubbleSort(arr) {
+  const array = [...arr];
+
   for (let i = 0; i < array.length - 1; ++i) {
     for (let j = 0; j < array.length - i - 1; ++j) {
       /*
